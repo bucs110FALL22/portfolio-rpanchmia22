7f6647f596f494b2449415015c68ed82fc068c7e
Roman Panchmia <112521388+rpanchmia22@users.noreply.github.com>
2022-09-15 20:07:37 +0000

    updated

---
+++ ch03/exercises/guess.py
@@ -0,0 +1,14 @@
+import random
+
+randnum = random.randint(1,10)
+
+for i in range(3):
+  guess = int(input("Pick a number from 1-10. "))
+  if(guess == randnum):
+    print("Correct!")
+  
+  elif(guess > randnum):
+    print("Incorrect! Too High.")
+
+  elif(guess < randnum):
+    print("Incorrect! Too Low.")
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,10 @@
 import turtle #1. import modules
 import random
+# import pygame
+# pygame.init()
+# window = pygame.display.set_mode()
+# import math
+
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -17,10 +22,26 @@ leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
+
 ## 5. Your PART A code goes here
+random = random.randrange(1,100)
+
+michelangelo.forward(random)
+leonardo.forward(random)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+for i in range(10):
+  michelangelo.forward(random)
+  leonardo.forward(random)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 
 # PART B - complete part B here
+# coords = []
+# num_sides = input("Choose a number of sides. ")
+# side_length = input("Choose the length of each side. ")
 
 
 window.exitonclick()
