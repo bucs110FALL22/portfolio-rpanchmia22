a20725da05a36c7a41cf7696f3a85cc38ed485fc
Roman Panchmia <112521388+rpanchmia22@users.noreply.github.com>
2022-09-19 20:44:14 +0000

    updated

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -6,9 +6,12 @@ for i in range(3):
   guess = int(input("Pick a number from 1-10. "))
   if(guess == randnum):
     print("Correct!")
-  
+    break
+    
   elif(guess > randnum):
     print("Incorrect! Too High.")
 
   elif(guess < randnum):
     print("Incorrect! Too Low.")
+
+print("You lose!")
