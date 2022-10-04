87a00eabc5915dbbbfebe7bf355f8b2d64f4e94f
Roman Panchmia <112521388+rpanchmia22@users.noreply.github.com>
2022-10-04 00:24:27 +0000

    updated

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,35 @@
+import turtle
+import random
+
+
+animal = turtle.Turtle()
+animal.color('green')
+animal.shape('turtle')
+animal.speed(1)
+
+
+width = 200
+height = 200
+window = turtle.Screen()
+window.screensize(width,height)
+xx = animal.xcor()
+yy = animal.ycor()
+
+
+def cointoss():
+    return random.choice(["Heads", "Tails"])
+
+
+while xx > -100 and xx < 100 and yy > -100 and yy < 100:
+  print(xx, yy)
+  xx = animal.xcor()
+  yy = animal.ycor()
+  if(cointoss() == "Heads"):
+    animal.left(90)
+    animal.forward(50)
+  if(cointoss() == "Tails"):
+    animal.right(90)
+    animal.forward(50)
+
+
+window.exitonclick()
