6263150d08d6efef2581ced1d68082d68f95a0d3
Roman Panchmia <112521388+rpanchmia22@users.noreply.github.com>
2022-10-14 16:14:10 +0000

    updated

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,6 +1,20 @@
-start = int(input("Pick a start number. "))
-upper_limit = int(input("Pick an end number. "))
+import pygame
+
+
+pygame.init()
+display = pygame.display.set_mode((400,400))
+color = pygame.Color('black')
+bg = pygame.Color("white")
+display.fill(bg)
+pygame.display.flip()
+font = pygame.font.Font(None, 50)
+start = 2
+upper_limit = 100
 iters = {}
+max_so_far = 0
+max_val = 0
+SCALE = 5
+
 
 for i in range(start, upper_limit+1):
   count = 0
@@ -11,6 +25,20 @@ for i in range(start, upper_limit+1):
     elif(i % 2 == 1):
       i = (i * 3 + 1)
     count = count + 1
-  iters[num] = count
-
+  iters[num * SCALE] = count * SCALE
+  if count > max_so_far:
+    max_so_far = count
+    max_val = num
+coordinates = list(iters.items())
 print(iters)
+print("Max value: " + str(max_val))
+print("Max iteration: " + str(max_so_far))
+
+
+pygame.draw.lines(display, color, False, coordinates)
+new_display = pygame.transform.flip(display, False, True)
+display.blit(new_display, (0,0))
+msg = font.render(str(max_so_far), True, color)
+display.blit(msg, (10,10))
+pygame.display.update()
+pygame.time.wait(5000)
