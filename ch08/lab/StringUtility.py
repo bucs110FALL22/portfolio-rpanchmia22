77edd35b53917c784abbebbbf905686aad5fb180
Roman Panchmia <112521388+rpanchmia22@users.noreply.github.com>
2022-11-11 22:34:43 +0000

    updated

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -1,13 +1,13 @@
 class StringUtility():
-  #INIT DEF#
+  #INIT DEF - Where all methods operate off of
   def __init__(self,string):
     self.str = string
 
-  #STR DEF---------------------------#
+  #STR DEF - Returns the string itself
   def __str__(self):
     return self.str
 
-  #VOWELS DEF----------------------------------------#
+  #VOWELS DEF - counts and returns the number of vowels in a string
   def vowels(self):
     vcount = 0
     for i in f"{self.str}":
@@ -28,11 +28,11 @@ class StringUtility():
     print(self.str,vcount)
     return str(vcount)
 
-  #BOTH ENDS DEF-------------------------------------#  
+  #BOTH ENDS - returns the first and last 2 characters
   def bothEnds(self):
     return f"{self.str[:2]}" + f"{self.str[-2:]}"
 
-  #FIX START DEF-------------------------------------#
+  #FIX START DEF - Changes every occurence of the first character to "*" except the first character
   def fixStart(self):
     self.fix = list(self.str)
     vow = 1
@@ -44,14 +44,14 @@ class StringUtility():
       vow += 1
     return nul.join(self.fix)
 
-  #ASCI SUM DEF-------------------------------------#
+  #ASCI SUM DEF - returns an int that is the sum of all ascii values in a string
   def asciiSum(self):
     sum = 0
     for i in self.str:
       sum = sum + ord(i)
     return sum
 
-  #CYPHER DEF---------------------------------------------------#
+  #CYPHER DEF - Encodes a string by shifting each letter 11 times to the right
   def cipher(self): 
     num = len(self.str)
     newstr = ''
